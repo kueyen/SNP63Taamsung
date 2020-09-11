896703b10735bc58698620d0935c64406ac1e8a4
Krit <moungnoi_k@silpakorn.edu>
2020-09-11 23:17:22 +0700

    seed fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            RestaurantSeeder::class,
+            CategorySeeder::class,
+            FoodSeeder::class
+        ]);
     }
 }
